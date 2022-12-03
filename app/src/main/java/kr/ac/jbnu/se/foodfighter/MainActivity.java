8ca2bac2c718fa3820a760cffd8ef964ac08786c
ryoo0930 <ryoo0930@naver.com>
2022-12-04 00:55:36 +0900

    MainActivity Point값 구현

--- app/src/main/java/kr/ac/jbnu/se/foodfighter/MainActivity.java
+++ app/src/main/java/kr/ac/jbnu/se/foodfighter/MainActivity.java
@@ -3,12 +3,25 @@ package kr.ac.jbnu.se.foodfighter;
 import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
     private static final int GPS_ENABLE_REQUEST_CODE = 2001;
     private static final int PERMISSIONS_REQUEST_CODE = 100;
     String[] REQUIRED_PERMISSIONS  = {Manifest.permission.ACCESS_FINE_LOCATION};
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
+
+
 
 
     @Override
@@ -56,6 +74,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        TextView point_text = (TextView) findViewById(R.id.point_text);
+        myRef.child("point").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                int userpoint = snapshot.getValue(int.class);
+                point_text.setText(userpoint + " P");
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
 
 
