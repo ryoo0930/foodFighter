053d5a2e2984871b65c020f2b9c969b3d483a740
ryoo0930 <ryoo0930@naver.com>
2022-12-04 16:12:03 +0900

    리뷰 리스트의 지역선택 구현

--- app/src/main/java/kr/ac/jbnu/se/foodfighter/ReviewListActivity.java
+++ app/src/main/java/kr/ac/jbnu/se/foodfighter/ReviewListActivity.java
@@ -8,6 +8,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -28,7 +29,8 @@ import java.util.List;
 public class ReviewListActivity extends AppCompatActivity {
 
     ListView listView;
-
+    String[] locationItems = {"중앙동", "풍남동", "노송동", "완산동", "서학동", "중화산동", "평화동", "서신동", "삼천동", "효자동", "진북동", "인후동", "덕진동", "금암동", "팔복동", "우아동", "호성동", "송천동", "조촌동", "여의동", "혁신동"};
+    String location = "덕진동";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,6 +39,7 @@ public class ReviewListActivity extends AppCompatActivity {
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference();
+
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
                 this,
@@ -50,23 +53,52 @@ public class ReviewListActivity extends AppCompatActivity {
 
         listView.setAdapter(adapter);
 
-        myRef.child("reviews").child("덕진동").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot postSnapshot: snapshot.getChildren()) {
-                    String reviewtitle = postSnapshot.getValue(Review.class).getTitle();
-                    adapter.add(reviewtitle);
+        //spinner=======
 
-                }
-                // list view update
-                adapter.notifyDataSetChanged();
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<String> spinneradapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item, locationItems
+        );
+
+        spinneradapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(spinneradapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                location = locationItems[i];
+
+                adapter.clear();
+
+                myRef.child("reviews").child(location).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot postSnapshot: snapshot.getChildren()) {
+                            String reviewtitle = postSnapshot.getValue(Review.class).getTitle();
+                            adapter.add(reviewtitle);
+
+                        }
+                        // list view update
+
+                        adapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        // TODO: not implemented
+                    }
+                });
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                // TODO: not implemented
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
             }
         });
+
+
+        //==================
+
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
